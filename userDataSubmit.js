68b80817ae9eacd7374289104b429218a89a0170
JYOTIPM1999 <jyotipm1999@gmail.com>
2026-09-15 17:51:41 +0530

    read file

--- userDataSubmit.js
+++ userDataSubmit.js
@@ -1,6 +1,20 @@
+const queryString = require("querystring");
 function userDataSubmit(req, res) {
+  let dataBody = [];
+  let showData;
+  req.on("data", (chunk) => {
+    dataBody.push(chunk);
+  });
+  req.on("end", () => {
+    let rawData = Buffer.concat(dataBody).toString();
+    let readableData = queryString.parse(rawData);
+    let dataString =
+      "My Name is " + readableData.name + "emial" + readableData.email;
+    showData = dataString;
+  });
   res.write(`
-    <h1>You got the data</h1>
-    `);
+      <h1>You got the data</h1>
+      `);
+  res.end(showData);
 }
 module.exports = userDataSubmit;
