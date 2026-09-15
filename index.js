b6d89bbccabcc4d2f082bf51a815d14b56aa6fd1
JYOTIPM1999 <jyotipm1999@gmail.com>
2026-09-15 19:23:21 +0530

    express html and routes implementation using express

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 // const express = require("express");
+// import express from "express";
+// import path from "path";
+// import home from "./pages/home.js";
+// import login from "./pages/login.js";
+// import submit from "./pages/submit.js";
+// const app = express();
+
+// app.get("/home", (req, res) => {
+//   res.send(home());
+// });
+// app.get("/", (req, res) => {
+//   res.send("hello ");
+// });
+// app.get("/login", (req, res) => {
+//   res.send(login());
+// });
+// app.post("/submit", (req, res) => {
+//   res.send(submit());
+// });
+// app.use((req, res) => {
+//   const absPath = path.resolve("pages/404.html");
+//   res.sendFile(absPath);
+// });
+// app.listen(3200);
+
 import express from "express";
-import home from "./pages/home.js";
-import login from "./pages/login.js";
-import submit from "./pages/submit.js";
 const app = express();
 
-app.get("/home", (req, res) => {
-  res.send(home());
-});
+function checkRoute(req, res, next) {
+  console.log(req.url);
+  next();
+}
+app.use(checkRoute);
+
 app.get("/", (req, res) => {
-  res.send("hello ");
+  res.send("Home");
 });
-app.get("/login", (req, res) => {
-  res.send(login());
+app.get("/users", (req, res) => {
+  res.send("Users");
 });
-app.post("/submit", (req, res) => {
-  res.send(submit());
+app.get("/products", (req, res) => {
+  res.send("Products");
 });
 app.listen(3200);
