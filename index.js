04e6055688bddef2d7620f4089de9cd8cf2da41d
JYOTIPM1999 <jyotipm1999@gmail.com>
2026-09-16 20:36:56 +0530

    Add MongoDB integration and update API routes for student management

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
 const app = express();
 
-app.get("/", (req, res) => {
-  let users = ["Abc", "efg", "xyz"];
-  let data = ``;
-  for (let i = 0; i < users.length; i++) {
-    data += `<li><a href="/users/${users[i]}">${users[i]}</a></li>`;
-  }
-  res.send(`Login Page<br><ul>${data}</ul>`);
-});
+const dbName = "schools";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
 
-app.get("/users/:name", (req, res) => {
-  res.send("Users Page");
-});
-app.get("/login", (req, res) => {
-  res.send("Login Page");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+client.connect().then((connection) => {
+  const db = connection.db(dbName);
+  app.get("/api", async (req, res) => {
+    const collection = db.collection("students");
+    const students = await collection.find().toArray();
+    res.send(students);
+  });
+  app.get("/add", (req, res) => {
+    res.send(`<form action="/submit" method="post">
+    <input type="text" placeholder="enter name" name="name"/>
+    <input type="text" placeholder="enter email" name="email"/>
+    <input type="text" placeholder="enter age" name="age"/>
+    <button>Submit</button>
+  </form>`);
+  });
+  app.post("/submit", async (req, res) => {
+    console.log(req.body);
+    const collection = db.collection("students");
+    const results = await collection.insertOne(req.body);
+    res.send(results);
+    res.end();
+  });
 
-app.get("/error", (req, res) => {
-  res.send("Error Page");
+  app.post("/add-students-api", async (req, res) => {
+    console.log(req.body);
+    const collection = db.collection("students");
+    const results = await collection.insertOne(req.body);
+    res.send(results);
+    res.end();
+  });
+  app.delete("/delete-students-api", async (req, res) => {
+    console.log(req.body);
+    const { email } = req.body;
+    const collection = db.collection("students");
+    const results = await collection.deleteOne({ email });
+    res.send(results);
+    res.end();
+  });
+  app.delete("/delete-students/:id", async (req, res) => {
+    console.log(req.params.id);
+    const collection = db.collection("students");
+    const results = await collection.deleteOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.send(results);
+  });
 });
-
-function errorHandler(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send("Try after some time");
-}
-
-app.use(errorHandler);
 
 app.listen(3200);
