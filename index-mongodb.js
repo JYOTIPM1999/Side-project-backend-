e424d1abe8e37b32b3529362a994526000ab9723
JYOTIPM1999 <jyotipm1999@gmail.com>
2026-09-17 17:35:41 +0530

    Implement MongoDB integration with Mongoose and update student management API

---
+++ index-mongodb.js
@@ -0,0 +1,83 @@
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+const app = express();
+
+const dbName = "schools";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+client.connect().then((connection) => {
+  const db = connection.db(dbName);
+  app.get("/api", async (req, res) => {
+    const collection = db.collection("students");
+    const students = await collection.find().toArray();
+    res.send(students);
+  });
+  app.get("/add", (req, res) => {
+    res.send(`<form action="/submit" method="post">
+    <input type="text" placeholder="enter name" name="name"/>
+    <input type="text" placeholder="enter email" name="email"/>
+    <input type="text" placeholder="enter age" name="age"/>
+    <button>Submit</button>
+  </form>`);
+  });
+  app.post("/submit", async (req, res) => {
+    console.log(req.body);
+    const collection = db.collection("students");
+    const results = await collection.insertOne(req.body);
+    res.send(results);
+    res.end();
+  });
+
+  app.post("/add-students-api", async (req, res) => {
+    console.log(req.body);
+    const collection = db.collection("students");
+    const results = await collection.insertOne(req.body);
+    res.send(results);
+    res.end();
+  });
+  app.delete("/delete-students-api", async (req, res) => {
+    console.log(req.body);
+    const { email } = req.body;
+    const collection = db.collection("students");
+    const results = await collection.deleteOne({ email });
+    res.send(results);
+    res.end();
+  });
+  app.delete("/delete-students/:id", async (req, res) => {
+    console.log(req.params.id);
+    const collection = db.collection("students");
+    const results = await collection.deleteOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.send(results);
+  });
+  app.delete("/delete-students/:name", async (req, res) => {
+    console.log(req.params.name);
+
+    const collection = db.collection("students");
+    const results = await collection.deleteOne({
+      name: req.params.name,
+    });
+    res.send(results);
+  });
+  app.put("/update-students/:id", async (req, res) => {
+    console.log(req.params.id);
+
+    const collection = db.collection("students");
+
+    const results = await collection.updateOne(
+      {
+        _id: new ObjectId(req.params.id),
+      },
+      {
+        $set: req.body,
+      },
+    );
+    res.send(results);
+  });
+});
+
+app.listen(3200);
--- index.js
+++ index.js
@@ -1,83 +1,36 @@
 import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import studentModel from "./model/studentModel.js";
+
 const app = express();
-
-const dbName = "schools";
-const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-client.connect().then((connection) => {
-  const db = connection.db(dbName);
-  app.get("/api", async (req, res) => {
-    const collection = db.collection("students");
-    const students = await collection.find().toArray();
-    res.send(students);
-  });
-  app.get("/add", (req, res) => {
-    res.send(`<form action="/submit" method="post">
-    <input type="text" placeholder="enter name" name="name"/>
-    <input type="text" placeholder="enter email" name="email"/>
-    <input type="text" placeholder="enter age" name="age"/>
-    <button>Submit</button>
-  </form>`);
-  });
-  app.post("/submit", async (req, res) => {
-    console.log(req.body);
-    const collection = db.collection("students");
-    const results = await collection.insertOne(req.body);
-    res.send(results);
-    res.end();
-  });
-
-  app.post("/add-students-api", async (req, res) => {
-    console.log(req.body);
-    const collection = db.collection("students");
-    const results = await collection.insertOne(req.body);
-    res.send(results);
-    res.end();
-  });
-  app.delete("/delete-students-api", async (req, res) => {
-    console.log(req.body);
-    const { email } = req.body;
-    const collection = db.collection("students");
-    const results = await collection.deleteOne({ email });
-    res.send(results);
-    res.end();
-  });
-  app.delete("/delete-students/:id", async (req, res) => {
-    console.log(req.params.id);
-    const collection = db.collection("students");
-    const results = await collection.deleteOne({
-      _id: new ObjectId(req.params.id),
-    });
-    res.send(results);
-  });
-  app.delete("/delete-students/:name", async (req, res) => {
-    console.log(req.params.name);
-
-    const collection = db.collection("students");
-    const results = await collection.deleteOne({
-      name: req.params.name,
-    });
-    res.send(results);
-  });
-  app.put("/update-students/:id", async (req, res) => {
-    console.log(req.params.id);
-
-    const collection = db.collection("students");
-
-    const results = await collection.updateOne(
-      {
-        _id: new ObjectId(req.params.id),
-      },
-      {
-        $set: req.body,
-      },
-    );
-    res.send(results);
-  });
+await mongoose.connect("mongodb://localhost:27017/schools").then(() => {
+  console.log("Database connected successfully");
 });
 
-app.listen(3200);
+app.get("/", async (req, res) => {
+  const student = await studentModel.find({});
+
+  res.json(student);
+});
+app.post("/save", async (req, res) => {
+  console.log(req.body);
+  const { name, age, email } = req.body;
+  if (!req.body || !name || !age || !email) {
+    return res.status(400).json({
+      message: "Please provide all required fields",
+      success: false,
+    });
+  }
+  const student = await studentModel.insertOne(req.body);
+
+  res.json({
+    message: "Student saved successfully",
+    success: true,
+    storedInfo: student,
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
